a066d09a35dfafc8cc6d951c75b44aeb86f6474c
Areg <aregghaz@gmail.com>
2025-07-06 10:20:39 +0400

    Update store.ts

--- src/store/store.ts
+++ src/store/store.ts
@@ -1,19 +1,25 @@
-import {configureStore} from "@reduxjs/toolkit";
-import menuReducer from './slices/menuSlice';
-import socReducer from './slices/socLinksSlice.tsx';
-import buttonReducer from './slices/buttonSlice';
-import mediaReducer from './slices/mediaSlice';
-import aboutReducer from './slices/aboutSlice';
+export type { AboutState }  from "./slices/aboutSlice"
+export type { ButtonState } from "./slices/buttonSlice"
+export type { MediaState }  from "./slices/mediaSlice"
+export type { MenuState }   from "./slices/menuSlice"
+export type { SocState }    from "./slices/socLinksSlice"
+
+import { configureStore } from "@reduxjs/toolkit"
+import aboutReducer        from "./slices/aboutSlice"
+import buttonReducer       from "./slices/buttonSlice"
+import mediaReducer        from "./slices/mediaSlice"
+import menuReducer         from "./slices/menuSlice"
+import socLinksReducer     from "./slices/socLinksSlice"
 
 export const store = configureStore({
-    reducer: {
-        menu: menuReducer,
-        soc: socReducer,
-        button: buttonReducer,
-        media: mediaReducer,
-        about: aboutReducer
-    }
-});
+  reducer: {
+    about:  aboutReducer,
+    button: buttonReducer,
+    media:  mediaReducer,
+    menu:   menuReducer,
+    soc:    socLinksReducer
+  }
+})
 
 export type RootState = ReturnType<typeof store.getState>;
 export type AppDispatch = typeof store.dispatch;
