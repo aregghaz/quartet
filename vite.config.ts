f2825a096cc4ef5150c301a1331f02fc05df5211
TigranGhazaryan24 <tghazaryan@curavel.com>
2025-07-07 10:49:23 +0400

    asd

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,47 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc';
+import path from 'node:path';
+import {defineConfig} from 'vite';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-  css: {
-    preprocessorOptions: {
-      scss: {
-        // additionalData: `@import "./src/styles/variables.scss";`,
-      },
+    publicDir: 'public',
+    plugins: [react()],
+    css: {
+        preprocessorOptions: {
+            scss: {
+                // additionalData: `@use "@src" as *;`,
+                includePaths: [path.resolve(__dirname, 'src')],
+                // sassOptions: {
+                //     javascriptEnabled: false,
+                // },
+            },
+        },
     },
-  },
-  assetsInclude: ['**/*.ttf']
-})
+    server: {
+        // proxy : {
+        //   '/api': {
+        //     target: 'https://dev02-service-getwey.azuremicroservices.io/api',
+        //     changeOrigin: true,
+        //     rewrite: (path) => path.replace(/^\/api/, ''),
+        //   },
+        // },
+        // host: true,
+        port: 3000,
+    },
+    optimizeDeps: {
+        exclude: ['js-big-decimal'],
+    },
+    define: {
+        global: 'globalThis', // this was added for resolving "sockjs-client" error
+    },
+    resolve: {
+        alias: {
+            '@src': path.resolve(__dirname, './src/'),
+            '@components': path.resolve(__dirname, './src/components/'),
+            '@images': path.resolve(__dirname, './src/images/'),
+            '@store': path.resolve(__dirname, './src/store/'),
+            '@svg': path.resolve(__dirname, './src/icons/'),
+            '@type': path.resolve(__dirname, './src/types/'),
+        },
+    },
+});
